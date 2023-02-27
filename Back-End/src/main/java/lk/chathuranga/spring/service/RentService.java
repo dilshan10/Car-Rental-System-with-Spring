00d7a1c0434b89d8b12502177cfce42efdf94a15
dilshan10 <superdilshan999@gmail.com>
2023-02-27 10:23:30 +0530

    Add Rent service Method.

--- Back-End/src/main/java/lk/chathuranga/spring/service/RentService.java
+++ Back-End/src/main/java/lk/chathuranga/spring/service/RentService.java
@@ -1,4 +1,15 @@
 package lk.chathuranga.spring.service;
 
+import lk.chathuranga.spring.dto.RentDTO;
+
+import java.util.ArrayList;
+
 public interface RentService {
+    public void addRent(RentDTO dto);
+
+    public void updateRent(RentDTO dto);
+
+    public void deleteRent(RentDTO dto);
+
+    public ArrayList<RentDTO>LoadAllRent();
 }
