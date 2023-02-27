0abced694e606633f6a1942e53f8353f62d44680
dilshan10 <superdilshan999@gmail.com>
2023-02-27 09:25:18 +0530

    Add CustomerRepo And AdminRepo.

---
+++ Back-End/src/main/java/lk/chathuranga/spring/repo/AdminRepo.java
@@ -0,0 +1,4 @@
+package lk.chathuranga.spring.repo;
+
+public interface AdminRepo {
+}
---
+++ Back-End/src/main/java/lk/chathuranga/spring/repo/CustomerRepo.java
@@ -0,0 +1,7 @@
+package lk.chathuranga.spring.repo;
+
+import lk.chathuranga.spring.entity.Customer;
+
+
+public interface CustomerRepo extends JpaRepository<Customer,String>{
+}
