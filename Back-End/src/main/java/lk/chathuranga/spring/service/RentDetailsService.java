e53b4fc55efec1cdcaedc6d769324efde63ae0ff
dilshan10 <superdilshan999@gmail.com>
2023-02-27 10:21:37 +0530

    Add RentDetails service Method.

--- Back-End/src/main/java/lk/chathuranga/spring/service/RentDetailsService.java
+++ Back-End/src/main/java/lk/chathuranga/spring/service/RentDetailsService.java
@@ -1,4 +1,15 @@
 package lk.chathuranga.spring.service;
 
+import lk.chathuranga.spring.dto.RentDetailstDTO;
+
+import java.util.ArrayList;
+
 public interface RentDetailsService {
+    public void addRentDetails(RentDetailstDTO dto);
+
+    public void updateRentDetails(RentDetailstDTO dto);
+
+    public void deleteRentDetails(RentDetailstDTO dto);
+
+    public ArrayList<RentDetailstDTO>loadAllRentDetails();
 }
