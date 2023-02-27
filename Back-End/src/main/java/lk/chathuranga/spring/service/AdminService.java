65eb8027c047079fdb15a4f67b3091c57a4c9bda
dilshan10 <superdilshan999@gmail.com>
2023-02-27 09:52:59 +0530

    Add Admin Service Method.

--- Back-End/src/main/java/lk/chathuranga/spring/service/AdminService.java
+++ Back-End/src/main/java/lk/chathuranga/spring/service/AdminService.java
@@ -1,4 +1,15 @@
 package lk.chathuranga.spring.service;
 
+import lk.chathuranga.spring.dto.AdminDTO;
+
+import java.util.ArrayList;
+
 public interface AdminService {
+    public void addAdmin(AdminDTO dto);
+
+    public void upDateAdmin(AdminDTO dto);
+
+    public void deleteAdmin(AdminDTO dto);
+
+    public ArrayList<AdminDTO> loadAllAdmin();
 }
