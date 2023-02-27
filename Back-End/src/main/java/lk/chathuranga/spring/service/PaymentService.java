0c2f98fedfdb1bd98222e8570d8efbeb1bc1359b
dilshan10 <superdilshan999@gmail.com>
2023-02-27 10:13:23 +0530

    Add Payment service Method.

--- Back-End/src/main/java/lk/chathuranga/spring/service/PaymentService.java
+++ Back-End/src/main/java/lk/chathuranga/spring/service/PaymentService.java
@@ -1,4 +1,15 @@
 package lk.chathuranga.spring.service;
 
+import lk.chathuranga.spring.dto.PaymentDTO;
+
+import java.util.ArrayList;
+
 public interface PaymentService {
+    public void addPayment(PaymentDTO dto);
+
+    public void updatePayment(PaymentDTO dto);
+
+    public void deletePayement(PaymentDTO dto);
+
+    public ArrayList<PaymentDTO> loadAllPayment();
 }
