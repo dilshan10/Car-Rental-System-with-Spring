8084f5eb57a29c2530cc98889e419ee1a0244e07
dilshan10 <superdilshan999@gmail.com>
2023-02-27 10:15:39 +0530

    Add User service Method.

--- Back-End/src/main/java/lk/chathuranga/spring/service/UserService.java
+++ Back-End/src/main/java/lk/chathuranga/spring/service/UserService.java
@@ -1,4 +1,15 @@
 package lk.chathuranga.spring.service;
 
+import lk.chathuranga.spring.dto.UserDTO;
+
+import java.util.ArrayList;
+
 public interface UserService {
+    public void addUser(UserDTO dto);
+
+    public void updateUser(UserDTO dto);
+
+    public void deleteUser(UserDTO dto);
+
+    public ArrayList<UserDTO> loadAllUser();
 }
