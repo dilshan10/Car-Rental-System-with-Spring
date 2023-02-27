b6cdc4e81bf0c7da4249dbd561b7e855fe27631f
dilshan10 <superdilshan999@gmail.com>
2023-02-27 10:28:07 +0530

    Add Schedule service Method.

--- Back-End/src/main/java/lk/chathuranga/spring/service/ScheduleService.java
+++ Back-End/src/main/java/lk/chathuranga/spring/service/ScheduleService.java
@@ -1,4 +1,15 @@
 package lk.chathuranga.spring.service;
 
+import lk.chathuranga.spring.dto.ScheduleDTO;
+
+import java.util.ArrayList;
+
 public interface ScheduleService {
+    public void addSchedule(ScheduleDTO dto);
+
+    public void updateSchedule(ScheduleDTO dto);
+
+    public void deleteSchedule(ScheduleDTO dto);
+
+    public ArrayList<ScheduleDTO> loadAllSchedule();
 }
