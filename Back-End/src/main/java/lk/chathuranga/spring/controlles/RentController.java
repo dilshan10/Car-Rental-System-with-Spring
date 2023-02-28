10ab15b7496594f7aa19424912f38607d621f274
dilshan10 <superdilshan999@gmail.com>
2023-02-28 17:19:24 +0530

    Add Rent  Controller Method.

--- Back-End/src/main/java/lk/chathuranga/spring/controlles/RentController.java
+++ Back-End/src/main/java/lk/chathuranga/spring/controlles/RentController.java
@@ -1,11 +1,13 @@
 package lk.chathuranga.spring.controlles;
 
 
+import lk.chathuranga.spring.dto.RentDTO;
 import lk.chathuranga.spring.service.RentService;
+import lk.chathuranga.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @CrossOrigin
@@ -14,5 +16,31 @@ public class RentController {
 
     @Autowired
     private RentService service;
+
+    @PostMapping
+    public ResponseUtil saveRent(@ModelAttribute RentDTO dto){
+        service.addRent(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @PutMapping
+    public ResponseUtil updateRent(@ModelAttribute RentDTO dto){
+        service.updateRent(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @DeleteMapping(params = "id")
+    public ResponseUtil deleteRenty(String id){
+        service.deleteRent(id);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @GetMapping
+    public ResponseUtil getAllRent(){
+        ArrayList<RentDTO> allRents = service.LoadAllRent();
+        return new ResponseUtil("200","Success..!",allRents);
+
+    }
+
 
 }
