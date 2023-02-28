a25e6dc33c6d38705c7dbcfc596f823929c9bf16
dilshan10 <superdilshan999@gmail.com>
2023-02-28 16:08:51 +0530

    Add Driver Controller Method.

--- Back-End/src/main/java/lk/chathuranga/spring/controlles/DriverControllers.java
+++ Back-End/src/main/java/lk/chathuranga/spring/controlles/DriverControllers.java
@@ -1,10 +1,12 @@
 package lk.chathuranga.spring.controlles;
 
+import lk.chathuranga.spring.dto.DriverDTO;
 import lk.chathuranga.spring.service.DriverService;
+import lk.chathuranga.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @CrossOrigin
@@ -13,5 +15,29 @@ public class DriverControllers {
 
     @Autowired
     private DriverService service;
+
+    @PostMapping
+    public ResponseUtil saveDriver(@ModelAttribute DriverDTO dto){
+        service.addDriver(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @PutMapping
+    public ResponseUtil updateDriver(@ModelAttribute DriverDTO dto){
+        service.updateDriver(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @DeleteMapping(params = "is")
+    public ResponseUtil deleteDriver(String id){
+        service.deleteDriver(id);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @GetMapping
+    public ResponseUtil getAllDrivers(){
+        ArrayList<DriverDTO> allDrivers = service.loadAllDriver();
+        return new ResponseUtil("200","Success..!",allDrivers);
+    }
 
 }
