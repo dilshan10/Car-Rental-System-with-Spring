2498fc62b50403a7a4e0500eb33fbe7221cbdb53
dilshan10 <superdilshan999@gmail.com>
2023-02-28 15:26:15 +0530

    Add Admin Controller Method.

--- Back-End/src/main/java/lk/chathuranga/spring/controlles/AdminControllers.java
+++ Back-End/src/main/java/lk/chathuranga/spring/controlles/AdminControllers.java
@@ -4,10 +4,7 @@ import lk.chathuranga.spring.dto.AdminDTO;
 import lk.chathuranga.spring.service.AdminService;
 import lk.chathuranga.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -18,6 +15,24 @@ public class AdminControllers {
 
     @Autowired
     private AdminService service;
+
+    @PostMapping
+    public ResponseUtil saveAdmin(@ModelAttribute AdminDTO dto){
+        service.addAdmin(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @PutMapping
+    public ResponseUtil updateAdmin(@ModelAttribute AdminDTO dto){
+        service.updateAdmin(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @DeleteMapping(params = "id")
+    public ResponseUtil deleteAdmin(String id){
+        service.deleteAdmin(id);
+        return new ResponseUtil("200",id+" Deleted..!",null);
+    }
 
     @GetMapping
     public ResponseUtil getAllAddmin(){
