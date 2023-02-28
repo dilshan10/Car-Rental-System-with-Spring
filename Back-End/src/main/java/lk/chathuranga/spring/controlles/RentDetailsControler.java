f996688eb03192ae6a859f82d26b021612385657
dilshan10 <superdilshan999@gmail.com>
2023-02-28 17:05:11 +0530

    Add RentDetails Controller Method.

--- Back-End/src/main/java/lk/chathuranga/spring/controlles/RentDetailsControler.java
+++ Back-End/src/main/java/lk/chathuranga/spring/controlles/RentDetailsControler.java
@@ -1,11 +1,13 @@
 package lk.chathuranga.spring.controlles;
 
 
+import lk.chathuranga.spring.dto.RentDetailstDTO;
 import lk.chathuranga.spring.service.RentDetailsService;
+import lk.chathuranga.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @CrossOrigin
@@ -14,5 +16,29 @@ public class RentDetailsControler {
 
     @Autowired
     private RentDetailsService service;
+
+    @PostMapping
+    public ResponseUtil saveRentDetails(@ModelAttribute RentDetailstDTO dto){
+        service.addRentDetails(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @PutMapping
+    public ResponseUtil updateRentDetails(@ModelAttribute RentDetailstDTO dto){
+        service.updateRentDetails(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @DeleteMapping(params = "id")
+    public ResponseUtil deleteRentDetails(String id){
+        service.deleteRentDetails(id);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @GetMapping
+    public ResponseUtil getAllRentDetails(){
+        ArrayList<RentDetailstDTO> allRentDetails = service.loadAllRentDetails();
+        return new ResponseUtil("200","Success..!",allRentDetails);
+    }
 
 }
