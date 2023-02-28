1bdacc5738a32abe457b3fb1f8dc6ac894d83d32
dilshan10 <superdilshan999@gmail.com>
2023-02-28 16:53:42 +0530

    Add payment Controller Method.

--- Back-End/src/main/java/lk/chathuranga/spring/controlles/PaymentControllers.java
+++ Back-End/src/main/java/lk/chathuranga/spring/controlles/PaymentControllers.java
@@ -1,11 +1,13 @@
 package lk.chathuranga.spring.controlles;
 
 
+import lk.chathuranga.spring.dto.PaymentDTO;
 import lk.chathuranga.spring.service.PaymentService;
+import lk.chathuranga.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @CrossOrigin
@@ -14,5 +16,28 @@ public class PaymentControllers  {
 
     @Autowired
     private PaymentService service;
+
+    @PostMapping
+    public ResponseUtil savePayment(@ModelAttribute PaymentDTO dto){
+        service.addPayment(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+    @PutMapping
+    public ResponseUtil updatePaymnt(@ModelAttribute PaymentDTO dto){
+        service.updatePayment(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @DeleteMapping(params = "id")
+    public ResponseUtil deletePayment(String id){
+        service.deletePayement(id);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @GetMapping
+    public ResponseUtil loadAllPayment(){
+        ArrayList<PaymentDTO> allPayments = service.loadAllPayment();
+        return new ResponseUtil("200","Success",allPayments);
+    }
 
 }
