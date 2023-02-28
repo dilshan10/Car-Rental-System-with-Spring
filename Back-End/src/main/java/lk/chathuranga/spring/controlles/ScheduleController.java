f0267b4b88a1d0e81df3a8955d2b71f68b30a5b8
dilshan10 <superdilshan999@gmail.com>
2023-02-28 17:13:19 +0530

    Add Schedule Controller Method.

--- Back-End/src/main/java/lk/chathuranga/spring/controlles/ScheduleController.java
+++ Back-End/src/main/java/lk/chathuranga/spring/controlles/ScheduleController.java
@@ -1,10 +1,12 @@
 package lk.chathuranga.spring.controlles;
 
+import lk.chathuranga.spring.dto.ScheduleDTO;
 import lk.chathuranga.spring.service.ScheduleService;
+import lk.chathuranga.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @CrossOrigin
@@ -13,5 +15,29 @@ public class ScheduleController {
 
     @Autowired
     private ScheduleService service;
+
+    @PostMapping
+    public ResponseUtil saveSchedule(@ModelAttribute ScheduleDTO dto){
+        service.addSchedule(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @PutMapping
+    public ResponseUtil updateSchedule(@ModelAttribute ScheduleDTO dto){
+        service.updateSchedule(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @DeleteMapping(params = "id")
+    public ResponseUtil deleteSchedule(String id){
+        service.deleteSchedule(id);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @GetMapping
+    public ResponseUtil gelAllSchedule(){
+        ArrayList<ScheduleDTO> allSchedules = service.loadAllSchedule();
+        return new ResponseUtil("200","Success..!",allSchedules);
+    }
 
 }
