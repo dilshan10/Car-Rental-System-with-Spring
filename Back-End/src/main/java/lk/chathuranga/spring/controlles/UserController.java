6e98a7d770507f8de3d77e3cc512ac0c655f4c6a
dilshan10 <superdilshan999@gmail.com>
2023-02-28 15:57:55 +0530

    Add User Controller Method.

--- Back-End/src/main/java/lk/chathuranga/spring/controlles/UserController.java
+++ Back-End/src/main/java/lk/chathuranga/spring/controlles/UserController.java
@@ -1,10 +1,12 @@
 package lk.chathuranga.spring.controlles;
 
+import lk.chathuranga.spring.dto.UserDTO;
 import lk.chathuranga.spring.service.UserService;
+import lk.chathuranga.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @CrossOrigin
@@ -13,5 +15,29 @@ public class UserController {
 
     @Autowired
     private UserService service;
+
+    @PostMapping
+    public ResponseUtil saveUser(@ModelAttribute UserDTO dto){
+        service.addUser(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @PutMapping
+    public ResponseUtil updareUser(@ModelAttribute UserDTO dto){
+        service.addUser(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @DeleteMapping(params = "id")
+    public ResponseUtil deleteUser(String id){
+        service.deleteUser(id);
+        return new ResponseUtil("200","Success..!",null);
+    }
+
+    @GetMapping
+    public ResponseUtil getAllUsers(){
+        ArrayList<UserDTO> allUsers = service.loadAllUser();
+        return new ResponseUtil("200","Success..!",allUsers);
+    }
 
 }
