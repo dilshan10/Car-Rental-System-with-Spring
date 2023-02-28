6657dc0e2102812b71c282705fd473cdbfacd86a
dilshan10 <superdilshan999@gmail.com>
2023-02-28 15:42:23 +0530

    Add Cardetails Controller Method.

--- Back-End/src/main/java/lk/chathuranga/spring/controlles/CarControllers.java
+++ Back-End/src/main/java/lk/chathuranga/spring/controlles/CarControllers.java
@@ -1,10 +1,12 @@
 package lk.chathuranga.spring.controlles;
 
+import lk.chathuranga.spring.dto.CarDetailsDTO;
 import lk.chathuranga.spring.service.CarDetailsService;
+import lk.chathuranga.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 
 @RestController
@@ -15,6 +17,27 @@ public class CarControllers {
     @Autowired
     private CarDetailsService service;
 
+    @PostMapping
+    public ResponseUtil saveCarDetails(@ModelAttribute CarDetailsDTO dto){
+        service.addCarDetails(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
 
+    @PutMapping
+    public ResponseUtil updateCarDetails(@ModelAttribute CarDetailsDTO dto){
+        service.updateCarDetails(dto);
+        return new ResponseUtil("200","Success..!",null);
+    }
 
+    @DeleteMapping(params = "id")
+    public ResponseUtil deleteCarDetails(String id){
+        service.deleteCarDetails(id);
+        return new ResponseUtil("200",id+" Deleted..!",null);
+    }
+
+    @GetMapping
+    public ResponseUtil getAllCarDetails(){
+        ArrayList<CarDetailsDTO> allCardetails = service.loadAllCarDetails();
+        return new ResponseUtil("200","Success",allCardetails);
+    }
 }
